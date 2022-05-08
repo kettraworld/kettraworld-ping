6b35a4ae5a4c970068486f348268d8ec0af63d9c
Jon4s_ <88103711+yJon4ss@users.noreply.github.com>
2022-05-08 09:59:34 +0100

    fixing errors and better code practises (#2)
    * refactor: better code practises and switching from node fetch to undici
    
    * fix: when you try to create a web server and you're already using that port or dont that internet connection it gives you a express error

--- index.js
+++ index.js
@@ -1,60 +1,38 @@
-const fetch = require("node-fetch")
+const { fetch } = require("undici")
 const express = require("express")
 const app = express()
 const chalk = require('chalk');
-const log = console.log;
-
-
-const ping = async function(url, interval) {
-if(!url) return log(chalk.red(`[📶 kettraworld-ping ] Error: `) + `Você deve especificar o URL!`);
-
-  function isValidUrl(string) {
-    try {
-      new URL(string);
-    } catch (_) {
-      return false;  
-    }
-
-    return true;
-  }
-
-  if(isValidUrl(url) !== true || url.includes("<" || ">" || "<script>" || "</script>") || encodeURIComponent(url).includes("%3C" || "%3E" || "%20")) return log(chalk.red(`[📶 kettraworld-ping ] Error: `) + `Inválida URL (${url})!`);
-
-
-let int = interval || 60000
-
-setInterval(async () => {
-    const response = await fetch(url, {headers: {'User-Agent' : 'kettraworld-ping (NPM Package)'}}).catch(err => {
-      log(chalk.red(`[📶 kettraworld-ping ] Error: `) + `falha ao pinga ${url}: ${err}`);
-    });
-      log(chalk.green(`[📶 kettraworld-ping ] `) + `Ping executado com sucesso ${url} com status ${response.status} (${response.statusText})`);
-
-    status = response.status;
-}, int);
-
-const response = await fetch(url, {headers: {'User-Agent' : 'kettraworld-ping (NPM Package)'}}).catch(err => {
-      log(chalk.red(`[📶 kettraworld-ping ] Error: `) + `falha ao pinga ${url}: ${err}`);
-    });
-      log(chalk.green(`[📶 kettraworld-ping ] `) + `Ping executado com sucesso ${url} com status ${response.status} (${response.statusText})`);
-
-    status = response.status;
-
-}
-
-
-const webserver =  async function(port) {
-let status = 200
-app.get('*', (req, res) => {
-    res.status(status).send(`<code>📡 This project is using <a href="https://www.npmjs.com/package/kettraworld-ping">kettraworld-ping</a> NPM package.</code>`);
-});
-const p = port|| 3000;
-
-app.listen(p, () => {
-log(chalk.green(`[📶 kettraworld-ping ] `) + `Webserver está escutando na porta ${p}!`);
-});
-}
+const { log } = console;
 
 module.exports = {
-ping,
-webserver
+  async ping(url, interval) {
+    if(!url) return log(chalk.red(`[ kettraworld-ping ] You must specify the url`))
+    const isValidUrl = (str) => {
+      try{
+        new URL(str)
+      } catch {
+        return false
+      }
+      return true
+    }
+    if(!isValidUrl(url) || ["<", ">", "<script>", "</script>"].includes(url) || ["%3C", "%3E", "%20"].includes(encodeURIComponent(url))) return log(chalk.red('[ kettraworld-ping ] Error: Invalid url provided ' + `(${url})`))
+
+    const i = interval || 60000;
+
+    setInterval(() => {
+      const res = await fetch(url, {
+        headers : { 'User-Agent': 'kettraworld-ping (NPM Package)'}
+      }).catch((err) => log(chalk.red(`[ kettraworld-ping ] Error: Failed to ping ${url}: ${err}`)))
+      log(chalk.green(`[ kettraworld-ping ] Ping executed (${url}) with the status ${res.status}`))
+    }, i);
+  },
+  async webserver (port) {
+    const status = 200;
+    app.get('*', (req, res) => {
+    res.status(status).send(`<code>📡 This project is using <a href="https://www.npmjs.com/package/kettraworld-ping">kettraworld-ping</a> NPM package.</code>`);
+    })
+    app.listen(port=3000, () => log(chalk.green(`[ kettraworld-ping ] Webserver listening on the port ${port || 3000}`))).catch(() => {
+      log(chalk.red('[ kettraworld-ping ] There was a error while creating the web server, common issues:\nThe port is already in use\nNo internet connection'))
+    })
+  }
 }
